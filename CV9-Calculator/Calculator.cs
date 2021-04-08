14ca31b6f9ebd0b3cfa759c83576c5d14b68edaa
Ivo_Točený <xtocen00@vutbr.cz>
2021-04-08 16:23:50 +0200

    Not finished calculator

---
+++ CV9-Calculator/Calculator.cs
@@ -0,0 +1,172 @@
+﻿namespace CV9_Calculator
+{
+    internal class Calculator
+    {
+        public string Memory { get; set; } = "";
+        public string Display { get; set; } = "";
+
+        private double number1 = 0;
+        private double number2 = 0;
+        private double result = 0;
+
+        private enum State
+        {
+            FirstNum,
+            SecondNum,
+            Result
+        };
+
+        private enum Operation
+        {
+            Plus,
+            Minus,
+            Multi,
+            Divide
+        };
+
+
+        private State _state = State.FirstNum;
+        private Operation _oper;
+
+        public void NumButton(string input)
+        {
+            switch (input)
+            {
+                case "0":
+                    Display += input;
+                    break;
+                case "1":
+                    Display += input;
+                    break;
+                case "2":
+                    Display += input;
+                    break;
+                case "3":
+                    Display += input;
+                    break;
+                case "4":
+                    Display += input;
+                    break;
+                case "5":
+                    Display += input;
+                    break;
+                case "6":
+                    Display += input;
+                    break;
+                case "7":
+                    Display += input;
+                    break;
+                case "8":
+                    Display += input;
+                    break;
+                case "9":
+                    Display += input;
+                    break;
+                case ".":
+                    Display += input;
+                    break;
+            }
+        }
+
+        public void OperButton(string input)
+        {
+            
+            if (_state == State.SecondNum)
+            {
+                ResultButton();
+            }
+            if (_state == State.FirstNum)
+            {
+                double.TryParse(Display, out number1);
+            }
+
+            switch (input)
+            {
+                case "+":
+                    Display += input;
+                    _oper = Operation.Plus;
+                    break;
+                case "-":
+                    Display += input;
+                    _oper = Operation.Minus;
+                    break;
+                case "/":
+                    Display += input;
+                    _oper = Operation.Divide;
+                    break;
+                case "*":
+                    Display += input;
+                    _oper = Operation.Multi;
+                    break;
+            }
+            Memory = Display;
+            Display = "";
+            _state = State.SecondNum;
+        }
+
+        public void SystemButton(string input)
+        {
+            switch (input)
+            {
+                case "DEL":
+                    if (Display.Length > 0)
+                    {
+                        Display = Display.Substring(0, Display.Length - 1);
+                    }
+                    break;
+                case "C":
+                    Display = "";
+                    Memory = "";
+                    number1 = 0;
+                    number2 = 0;
+                    _state = State.FirstNum;
+                    break;
+                case "CE":
+                    Display = "";
+                    break;
+            }
+        }
+
+        public void ResultButton()
+        {
+            double.TryParse(Display, out number2);
+
+            switch (_oper)
+            {
+                case Operation.Plus:
+                    this.result = number1 + number2;
+                    Memory = "";
+                    Display = result.ToString();
+                    _state = State.FirstNum;
+                    break;
+                case Operation.Minus:
+                    this.result = number1 - number2;
+                    Memory = "";
+                    Display = result.ToString();
+                    _state = State.FirstNum;
+                    break;
+                case Operation.Multi:
+                    this.result = number1 * number2;
+                    Memory = "";
+                    Display = result.ToString();
+                    _state = State.FirstNum;
+                    break;
+                case Operation.Divide:
+                    if (number2 == 0)
+                    {
+                        Memory = "Invalid operation dividing 0";
+                        Display = "";
+                        number1 = 0;
+                        number2 = 0;
+                        _state = State.FirstNum;
+                        break;
+                    }
+                    this.result = number1 / number2;
+                    Memory = "";
+                    Display = result.ToString();
+                    _state = State.FirstNum;
+                    break;
+            }
+        }
+    }
+}
---
+++ CV9-Calculator/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CV9_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            display.Content = calculator.Display;
+            memory.Content = calculator.Memory;
+        }
+        private void NumButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.NumButton((sender as Button).Content.ToString());
+            display.Content = calculator.Display;
+            memory.Content = calculator.Memory;
+        }
+
+        private void OperButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.OperButton((sender as Button).Content.ToString());
+            display.Content = calculator.Display;
+            memory.Content = calculator.Memory;
+        }
+
+        private void SystemButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.SystemButton((sender as Button).Content.ToString());
+            display.Content = calculator.Display;
+            memory.Content = calculator.Memory;
+        }
+
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.ResultButton();
+            display.Content = calculator.Display;
+            memory.Content = calculator.Memory;
+        }
+    }
+
+}
